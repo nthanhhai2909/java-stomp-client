d35187df79c21806460bc816fe7ee3ceed767465
hai.t.nguyen <hai.t.nguyen@opswat.com>
2022-10-08 22:06:39 +0700

    update some example

---
+++ src/main/java/com/opswat/mem/StompMain.java
@@ -0,0 +1,23 @@
+package com.opswat.mem;
+
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+import java.util.Scanner;
+
+public class StompMain {
+
+    public static void main(String[] argv) {
+        WebSocketClient client = new StandardWebSocketClient();
+        WebSocketStompClient stompClient = new WebSocketStompClient(client);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSessionHandler sessionHandler = new MyStompSessionHandler();
+        stompClient.connect("ws://localhost:8999/ws", sessionHandler);
+
+        new Scanner(System.in).nextLine(); // Don't close immediately.
+    }
+}
